ed4f482e2910a11eb0d24f851867d3ab5fe02b51
Roey Darwish Dror <roey.ghost@gmail.com>
2016-11-10 08:37:19 +0200

    Initial commit

---
+++ chooseforme.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+from zlib import adler32
+from bs4 import BeautifulSoup
+
+def get_tweets(user):
+    response = requests.get("http://twitter.com/" + user)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup.find(class_="js-tweet-text-container").text.strip()
+
+
+def _choice(tweet, options):
+    data = tweet + ",".join(options)
+    return adler32(data.encode("UTF-8")) % len(options)
+
+
+def main():
+    options = sys.argv[1:]
+    if not options:
+        print("Error: Division by the number of fucks Oren Hazan gives")
+        print("Usage: chooseforme.py [option ...]")
+        return 1
+
+    options.sort()
+    tweet = get_tweets("oren_haz")
+    print(options[_choice(tweet, options)])
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
